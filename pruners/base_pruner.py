eac6c2d9437f5d800baaa49c64c954e686d8a0d4
Bhavesh Kalisetti <bhaveshkalisetti@gmail.com>
2025-12-30 23:07:19 -0800

    fastv pruner

---
+++ pruners/base_pruner.py
@@ -0,0 +1,23 @@
+import torch
+
+from abc import ABC
+
+class Pruner(ABC):
+    """
+    Base class for a token pruner.
+    A VLM takes in a pruner on initialization and uses prune() to reduce the number of tokens with each layer.
+    """
+    def __init__(self):
+        pass
+
+    def prune(
+        self,
+        layer_idx: int,
+        attention_scores: torch.Tensor,
+        token_types: torch.Tensor,
+    ):
+        """
+        prune() will be called in each decoder layer's forward pass.
+        this turns the decoder layer into a function that maps (B, T_1, D) -> (B, T_2, D) where T_2 <= T_1.
+        """
+        pass
---
+++ pruners/fastv_pruner.py
@@ -0,0 +1,56 @@
+import torch
+
+from base_pruner import Pruner
+
+class FastVPruner(Pruner):
+    def __init__(
+        self,
+        layer_threshold,
+        filtering_ratio,
+    ):
+        """
+        layer_threshold: if the layer # is less than this, no pruning is done
+        filtering_ratio: float between 0 and 1, the fraction of tokens to remove at each layer
+        """
+        super().__init__()
+
+        self.layer_threshold = layer_threshold
+        self.filtering_ratio = filtering_ratio
+
+
+    def prune(
+        self,
+        layer_idx,
+        hidden_states,
+        attention_scores,
+        token_types,
+    ):
+        """
+        Prunes tokens using FastV: at each layer, keep the tokens with the highest average attention score received from all other tokens.
+        attention_scores is a tensor of shape (M, T, T), where M is the number of heads and T is the sequence length.
+        token_types is a tensor of shape (T,) with value 0 for text tokens and 1 for visual tokens.
+        """
+        if layer_idx < self.layer_threshold:
+            return hidden_states, token_types
+        T = len(token_types)
+        V = int(token_types.sum())
+
+
+        # we are going to:
+        # - average across heads
+        # - ignore how much each token attends to itself (was in the phrasing of the FastV paper, not sure if this is correct).
+        # - find indices of topk visual tokens by average attention score received from all other tokens.
+        # - build a keep mask that is True for the tokens to keep (text tokens and topk visual tokens) and False otherwise.
+        # - index into hidden states with the keep mask.
+        attention_scores = attention_scores.mean(dim=0)
+        attention_scores *= (1 - torch.eye(T, device=attention_scores.device))
+        visual_token_scores = attention_scores[:, token_types == 1].mean(dim=0)
+
+        visual_indices = token_types.nonzero(as_tuple=True)[0]
+
+        amount_to_keep = int(V * (1 - self.filtering_ratio))
+        topk_relative = visual_token_scores.topk(amount_to_keep).indices.sort().values
+
+        keep_mask = ~token_types.bool()
+        keep_mask[visual_indices[topk_relative]] = True
+        return hidden_states[:, keep_mask, :], token_types[keep_mask]
