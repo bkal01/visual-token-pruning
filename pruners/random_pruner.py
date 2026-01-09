667dfe18862608656cfce7cfb736b028214542c1
Bhavesh Kalisetti <bhaveshkalisetti@gmail.com>
2026-01-09 13:33:47 -0800

    fix random pruner

--- pruners/random_pruner.py
+++ pruners/random_pruner.py
@@ -5,11 +5,10 @@ from pruners.base_pruner import Pruner
 class RandomPruner(Pruner):
     def __init__(
         self,
-        layer_threshold,
+        target_layers,
         filtering_ratio,
     ):
-        super().__init__()
-        self.layer_threshold = layer_threshold
+        super().__init__(target_layers)
         self.filtering_ratio = filtering_ratio
 
     def prune(
@@ -22,12 +21,13 @@ class RandomPruner(Pruner):
         """
         Prunes visual tokens uniformly at random.
         """
-        if layer_idx < self.layer_threshold:
-            return hidden_states, token_types
+        T = attention_scores.shape[0]
         V = int(token_types.sum())
+        if layer_idx not in self.target_layers:
+            return hidden_states, torch.ones(T, dtype=torch.bool, device=token_types.device)
 
         amount_to_keep = int(V * (1 - self.filtering_ratio))
         visual_indices = token_types.nonzero(as_tuple=True)[0]
         keep_mask = ~token_types.bool()
         keep_mask[visual_indices[torch.randperm(V, device=token_types.device)[:amount_to_keep]]] = True
-        return hidden_states[:, keep_mask, :], token_types[keep_mask]
+        return hidden_states[:, keep_mask, :], keep_mask
